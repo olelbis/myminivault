906485f7c4df48958503d81a8866fbf56a2bd191
olelbis <marco.gianini.it@gmail.com>
2026-07-11 00:06:27 +0200

    Prepare v0.12.1 release

--- cmd/vault/config_cli.go
+++ cmd/vault/config_cli.go
@@ -23,7 +23,7 @@ const (
 	sharedTokenVaultName = "shared-token-vault.json"
 	lockFileName         = ".myminivault.lock"
 	saltSize             = 16
-	vaultVersion         = "0.12.0"
+	vaultVersion         = "0.12.1"
 )
 
 var (
